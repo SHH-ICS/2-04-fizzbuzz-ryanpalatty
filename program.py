d715e01b82fa962079247c94f444b271ce65b0a8
ryanpalatty <ryan.palatty@stu.ocsb.ca>
2024-10-10 00:23:31 +0000

    fizzbuzz code

--- program.py
+++ program.py
@@ -6,6 +6,27 @@
 # For multiples of 15, output   FizzBuzz
 # End at 32
 
+
+#this is what i came up with
+
+ for myNumber in range(1, 32):
+    if(myNumber % 3 == 0 and myNumber % 5 == 0):
+        print("FizzBuzz") 
+    elif(myNumber % 3 == 0):
+        print("Fizz")
+    elif(myNumber % 5 == 0):
+        print("Buzz")
+    else:
+        print(myNumber)
+
+
+
+
+# This is was what came in the page
+
+for X in range(1, 32):
+
+
 result = ""
 for myNumber in range(32):
   result = str(result) + str(myNumber) + "\n"
